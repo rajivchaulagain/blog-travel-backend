6739be2e478c418179ea5a2788f8295643dccd48
Rajiv Chaulagain <82865638+rajivchaulagain@users.noreply.github.com>
2022-10-15 22:40:51 +0545

    edited package.json

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ app.use('/' , express.static('public'));
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('./client/build'));
     app.get('*' , (req , res) => {
-        res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'));
+        res.sendFile(path.resolve(__dirname , "./client/build", "index.html"));
     });
 }
 
